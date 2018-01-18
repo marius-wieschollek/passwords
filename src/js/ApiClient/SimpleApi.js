aa6ced9c0e3c1a366e9f33ea7109ecd2f756bdaa
Marius David Wieschollek <passwords.public@mdns.eu>
2018-01-18 13:32:04 +0100

    Clean up simple api

--- src/js/ApiClient/SimpleApi.js
+++ src/js/ApiClient/SimpleApi.js
@@ -592,26 +592,6 @@ export default class SimpleApi {
         });
     }
 
-    buildFormData(formData, data, parentKey) {
-        if(data && typeof data === 'object' && !(data instanceof Date) && !(data instanceof File)) {
-            Object.keys(data).forEach(key => {
-                this.buildFormData(formData, data[key], parentKey ? `${parentKey}[${key}]`:key);
-            });
-        } else {
-            const value = data == null ? '':data;
-
-            formData.append(parentKey, value);
-        }
-    }
-
-    jsonToFormData(data) {
-        const formData = new FormData();
-
-        this.buildFormData(formData, data);
-
-        return formData;
-    }
-
     /**
      *
      * @param url
