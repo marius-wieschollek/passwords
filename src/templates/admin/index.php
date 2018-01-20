ce845bdfb5c31271e7adb4d68837b32788d224a9
Marius David Wieschollek <passwords.public@mdns.eu>
2018-01-20 13:52:53 +0100

    Fixed missing utility script on admin page

--- src/templates/admin/index.php
+++ src/templates/admin/index.php
@@ -5,7 +5,7 @@ use OCA\Passwords\AppInfo\Application;
 
 /** @var $_ array */
 
-script('passwords', ['Helper/utility', 'Static/common', 'Static/admin']);
+script('passwords', ['Static/utility', 'Static/common', 'Static/admin']);
 style(Application::APP_NAME, 'admin');
 
 print_unescaped($this->inc('admin/settings'));
