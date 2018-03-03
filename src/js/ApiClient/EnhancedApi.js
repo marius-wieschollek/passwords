046b6e5f543ff4f43c595617cfa826b6d8d53351
Marius David Wieschollek <passwords.public@mdns.eu>
2018-03-03 21:21:04 +0100

    Fixed domain regex

--- src/js/ApiClient/EnhancedApi.js
+++ src/js/ApiClient/EnhancedApi.js
@@ -853,8 +853,8 @@ export default class EnhancedApi extends SimpleApi {
                 domain = (i===2 ? '':'.') + part + domain;
             }
         }
-        let subdomains = ['m', 'www', 'www2', 'mail', 'email', 'login', 'signin', 'profile', 'account',  navigator.language],
-            regex = RegExp('^(' + subdomains.join('|') + ')\.');
+        let subdomains = ['m', 'en', 'www', 'www2', 'mail', 'email', 'login', 'signin', 'profile', 'account',  navigator.language],
+            regex = RegExp('^(' + subdomains.join('|') + ')\\.');
 
         return domain.replace(regex, '');
     }
