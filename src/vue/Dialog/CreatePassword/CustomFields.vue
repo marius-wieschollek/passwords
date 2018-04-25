b9f401c73a4f8f383d2ca4d7ab19ade928cbb035
Marius David Wieschollek <passwords.public@mdns.eu>
2018-04-25 23:40:01 +0200

    Improved Custom Fields Form

--- src/vue/Dialog/CreatePassword/CustomFields.vue
+++ src/vue/Dialog/CreatePassword/CustomFields.vue
@@ -1,22 +1,28 @@
 <template>
-    <div class="custom-fields">
-        <div class="custom-field-form">
-            <input type="text" placeholder="Name" v-model="field.name" maxlength="48">
-            <select v-model="field.type" v-if="isValidName">
+    <div class="custom-fields" id="custom-fields">
+        <div v-for="(field, name) in fields">{{name}} {{field.value}}</div>
+
+        <form class="custom-field-form">
+            <input type="text" placeholder="Name" class="field-name" v-model="field.name" maxlength="48">
+            <select class="field-type" v-model="field.type" :disabled="!isValidName">
                 <option value="text">Text</option>
                 <option value="password">Password</option>
                 <option value="email">E-Mail</option>
                 <option value="url">Link</option>
                 <option value="file">File</option>
             </select>
-            <input :type="getFieldType" placeholder="Value" v-model="field.value" maxlength="320" v-if="isValidName">
-        </div>
+            <button class="fa fa-folder file-picker" @click="openNextcloudFile" v-if="showFilePicker" :disabled="!isValidName">{{field.value}}</button>
+            <input class="field-value" :type="getFieldType" placeholder="Value" v-model="field.value" maxlength="320" v-if="!showFilePicker" :disabled="!isValidName"/>
+            <button class="fa fa-plus button-save" @click="addCustomField" :disabled="!field.value || !field.value.length"></button>
+        </form>
     </div>
 </template>
 
 <script>
+    import Messages from '@js/Classes/Messages';
+
     export default {
-        props: {
+        props   : {
             fields: {
                 type: Object
             }
@@ -24,25 +30,67 @@
         data() {
             return {
                 field: {
-                    name:'',
-                    type:'text',
-                    value:null
+                    name : '',
+                    type : 'text',
+                    value: null
                 },
-            }
+                file : null
+            };
         },
         computed: {
             isValidName() {
-                return !this.fields.hasOwnProperty(this.field.name) && this.field.name.length && this.field.name.substr(0,1) !== '_';
+                return !this.fields.hasOwnProperty(this.field.name) && this.field.name.length && this.field.name.substr(0, 1) !== '_';
             },
             getFieldType() {
-                if(this.field.type === 'password') return 'password';
-                if(this.field.type === 'email') return 'email';
+                if(['password', 'email'].indexOf(this.field.type) !== -1) return this.field.type;
                 return 'text';
+            },
+            showFilePicker() {
+                return this.isValidName && this.field.type === 'file';
+            }
+        },
+        methods : {
+            async openNextcloudFile() {
+                this.field.value = await Messages.filePicker();
+            },
+            addCustomField() {
+                this.fields[this.field.name] = {
+                    type: this.field.type,
+                    value: this.field.value,
+                };
+                this.field = {
+                    name : '',
+                    type : 'text',
+                    value: null
+                };
             }
         }
     };
 </script>
 
-<style scoped>
+<style lang="scss">
+    #app-popup #passwords-create-new #custom-fields {
+        .custom-field-form {
+            .field-name {
+                width     : 75.5%;
+                max-width : none;
+            }
 
+            .field-type {
+                width     : 23%;
+                max-width : none;
+            }
+
+            .field-value,
+            .file-picker {
+                width     : 92.5%;
+                max-width : none;
+            }
+
+            .button-save {
+                width     : 6%;
+                max-width : none;
+            }
+        }
+    }
 </style>
