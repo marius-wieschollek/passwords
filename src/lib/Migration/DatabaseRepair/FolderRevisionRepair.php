01d21941bacfb9089d7c6b47c93c444d50138ed8
Marius David Wieschollek <passwords.public@mdns.eu>
2018-07-04 10:36:12 +0200

    Added folder relation check in repair steps

--- src/lib/Migration/DatabaseRepair/FolderRevisionRepair.php
+++ src/lib/Migration/DatabaseRepair/FolderRevisionRepair.php
@@ -3,7 +3,12 @@
 namespace OCA\Passwords\Migration\DatabaseRepair;
 
 use OCA\Passwords\Db\FolderMapper;
+use OCA\Passwords\Db\FolderRevision;
+use OCA\Passwords\Db\RevisionInterface;
 use OCA\Passwords\Services\Object\FolderRevisionService;
+use OCA\Passwords\Services\Object\FolderService;
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\AppFramework\Db\MultipleObjectsReturnedException;
 
 /**
  * Class FolderRevisionRepairHelper
@@ -25,5 +30,28 @@ class FolderRevisionRepair extends AbstractRevisionRepair {
      */
     public function __construct(FolderMapper $modelMapper, FolderRevisionService $revisionService) {
         parent::__construct($modelMapper, $revisionService);
+    }
+
+    /**
+     * @param FolderRevision|RevisionInterface $revision
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function repairRevision(RevisionInterface $revision): bool {
+        $fixed = false;
+
+        if($revision->getParent() !== FolderService::BASE_FOLDER_UUID) {
+            try {
+                $this->modelMapper->findByUuid($revision->getParent());
+            } catch(DoesNotExistException | MultipleObjectsReturnedException $e) {
+                $revision->setParent(FolderService::BASE_FOLDER_UUID);
+                $fixed = true;
+            }
+        }
+
+        if($fixed) $this->revisionService->save($revision);
+
+        return $fixed || parent::repairRevision($revision);
     }
 }
