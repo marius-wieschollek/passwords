edc81ba2b86927a8acd75ee9f177b63ef5669944
Marius David Wieschollek <passwords.public@mdns.eu>
2018-08-27 13:48:44 +0200

    [GitHub #62] Fixed some layout bugs in NC 14

--- src/vue/Components/Breadcrumb.vue
+++ src/vue/Components/Breadcrumb.vue
@@ -21,7 +21,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="passwords-more-menu" v-if="showAddNew" :class="{active: showMoreMenu}">
+            <div class="passwords-more-menu actions" v-if="showAddNew" :class="{active: showMoreMenu}">
                 <span class="button new" @click="toggleMoreMenu"><span class="icon icon-add"></span></span>
                 <div class="popovermenu menu menu-center" @click="toggleMoreMenu">
                     <ul>
