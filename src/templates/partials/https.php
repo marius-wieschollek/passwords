e3e3b392588de9c4ada37db8b3a971885906e177
Marius Wieschollek <passwords.public@mdns.eu>
2018-10-23 09:55:19 +0200

    Add link to reverse proxy configuration in HTTPS setup

--- src/templates/partials/https.php
+++ src/templates/partials/https.php
@@ -21,13 +21,15 @@ $messageHead   = $l->t('This application requires %s in order to work safely.', 
 $messageReload = $l->t('You can try to %s the page with HTTPS.', ["<a href=\"{$linkReload}\">{$reloadPage}</a>"]);
 
 if($isAdmin) {
-    $linkDocumentation = 'https://docs.nextcloud.com/server/13/admin_manual/configuration_server/harden_server.html#use-https';
+    $linkDocumentation = 'https://docs.nextcloud.com/server/14/admin_manual/configuration_server/harden_server.html#use-https';
+    $linkReverseProxy  = 'https://docs.nextcloud.com/server/14/admin_manual/configuration_server/reverse_proxy_configuration.html';
     $linkCertificate   = 'https://letsencrypt.org/getting-started/';
     $linkSettings      = \OC::$server->getURLGenerator()->getAbsoluteURL('/index.php/settings/admin/passwords#passwords-https-detection');
 
     $readDocumentation  = $l->t('documentation');
     $messageDebugging   = $l->t('Enable %s to troubleshoot this error.', ["<a href=\"{$linkSettings}\" target=\"_blank\">HTTPS debugging</a>"]);
     $messageConfigure   = $l->t('Consult the %s to configure Nextcloud correctly.', ["<a href=\"{$linkDocumentation}\" target=\"_blank\">{$readDocumentation}</a>"]);
+    $messageProxy       = $l->t('If you are using an HTTPS proxy, please read this %s.', ["<a href=\"{$linkReverseProxy}\" target=\"_blank\">{$readDocumentation}</a>"]);
     $messageCertificate = $l->t('If you do not have a HTTPS certificate, get one for free from %s.', ["<a href=\"{$linkCertificate}\" target=\"_blank\">Let's Encrypt</a>"]);
 } else {
     $messageAdmin = $l->t('If this problem persists, you should contact your administrator.');
@@ -46,6 +48,7 @@ if($isAdmin) {
                 <ul>
                     <li><?php print_unescaped($messageDebugging); ?></li>
                     <li><?php print_unescaped($messageConfigure); ?></li>
+                    <li><?php print_unescaped($messageProxy); ?></li>
                     <li><?php print_unescaped($messageCertificate); ?></li>
                 </ul>
             <?php else: ?>
