2971512696b0918ca44a3122715a6d11b4fec87a
Marius David Wieschollek <passwords.public@mdns.eu>
2018-11-18 14:27:18 +0100

    Create groups during docker setup

--- docker/php/files/setup.sh
+++ docker/php/files/setup.sh
@@ -11,11 +11,16 @@ su -p www-data -s /bin/sh -c "
 /var/www/html/occ config:system:set mail_smtpport --value=1025 --type=int;
 /var/www/html/occ app:enable passwords;
 export OC_PASS=PasswordsApp;
+/var/www/html/occ group:add users
+/var/www/html/occ group:add random
+/var/www/html/occ group:adduser users admin
 /var/www/html/occ user:setting admin settings email admin@passwords.app
 /var/www/html/occ user:add max --display-name='Max Mustermann' --password-from-env;
 /var/www/html/occ user:setting max settings email max@passwords.app
+/var/www/html/occ group:adduser users max
 /var/www/html/occ user:add erika --display-name='Erika Mustermann' --password-from-env;
 /var/www/html/occ user:setting erika settings email erika@passwords.app
+/var/www/html/occ group:adduser users erika
 /var/www/html/occ config:app:set passwords service/favicon/bi/url --value=http://passwords-iconserver:7070/icon;
 /var/www/html/occ config:app:set passwords service/favicon --value=bi;
 /var/www/html/occ config:app:set passwords service/preview --value=pageres;
@@ -38,7 +43,8 @@ until [[  ${COUNTER} -lt 1 ]]; do
 
         su -p www-data -s /bin/bash -c "export OC_PASS=PasswordsApp;
         /var/www/html/occ user:add '${LOGIN}' --display-name='${FIRSTNAME} ${LASTNAME}' --password-from-env;
-        /var/www/html/occ user:setting '${LOGIN}' settings email ${LOGIN}@passwords.app"
+        /var/www/html/occ user:setting '${LOGIN}' settings email ${LOGIN}@passwords.app
+        /var/www/html/occ group:adduser random '${LOGIN}'"
 
         let COUNTER-=1
     fi
