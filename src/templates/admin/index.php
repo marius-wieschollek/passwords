a4cc0cccc04d562eabbe9e470ac0e42a816e88cb
Marius David Wieschollek <passwords.public@mdns.eu>
2019-02-04 22:04:06 +0100

    Removed debugHTTPS check in admin settings

--- src/templates/admin/index.php
+++ src/templates/admin/index.php
@@ -13,9 +13,4 @@ use OCA\Passwords\AppInfo\Application;
 script('passwords', ['Static/utility', 'Static/common', 'Static/admin']);
 style(Application::APP_NAME, 'admin');
 
-if($_['debugHTTPS']) {
-    $_['https'] = $_['support']['https'];
-    print_unescaped($this->inc('partials/https_debug', $_));
-}
-
 print_unescaped($this->inc('admin/settings'));
