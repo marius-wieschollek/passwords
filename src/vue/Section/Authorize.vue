dd57198b85d587a840b9b32b687978f7d3110f9c
Marius David Wieschollek <passwords.public@mdns.eu>
2019-02-18 23:33:47 +0100

    Fixed ui glitches in login screen

--- src/vue/Section/Authorize.vue
+++ src/vue/Section/Authorize.vue
@@ -1,14 +1,16 @@
 <template>
     <div id="app-content">
-        <form id="authorize-window" @submit="submitLogin">
+        <form id="authorize-window" @submit="submitLogin" :class="getClasses">
             <translate tag="div" icon="user" class="login-message" say="You are logging in as {user}" :variables="loginVars" v-if="impersonating"/>
-            <translate icon="repeat" title="Request Token again" :iconClass="retryClass" @click="requestToken()" v-if="retryVisible"/>
-            <div class="passwords">
-                <input type="password" placeholder="Password" v-model="password" required v-if="hasPassword">
-                <select v-model="providerId" v-if="hasToken && providers.length > 1">
-                    <option v-for="(option, id) in providers" :value="id" :title="option.description">{{option.label}}</option>
-                </select>
-                <input type="text" placeholder="Token" :title="provider.description" v-model="token" required v-if="hasToken && provider.type === 'user-token'">
+            <div class="login-container">
+                <translate icon="repeat" title="Request Token again" :iconClass="retryClass" @click="requestToken()" v-if="retryVisible"/>
+                <div>
+                    <input type="password" placeholder="Password" v-model="password" required v-if="hasPassword">
+                    <select v-model="providerId" v-if="hasToken && providers.length > 0">
+                        <option v-for="(option, id) in providers" :value="id" :title="option.description">{{option.label}}</option>
+                    </select>
+                    <input type="text" placeholder="Token" :title="provider.description" v-model="token" required v-if="hasToken && provider.type === 'user-token'">
+                </div>
             </div>
             <input type="submit" value="Login" v-if="hasToken || hasPassword">
             <translate class="login-error" :say="errorMessage" tag="div" v-if="hasError"/>
@@ -18,7 +20,6 @@
 
 <script>
     import API from '@js/Helper/api';
-    import EnhancedApi from '@js/ApiClient/EnhancedApi';
     import Messages from '@js/Classes/Messages';
     import Translate from '@vue/Components/Translate';
 
@@ -42,7 +43,7 @@
         },
 
         created() {
-            document.body.classList.add('pw-authorisation');
+            document.body.classList.add('pw-authorize');
             API.requestSession()
                .then((d) => {
                    if(d.hasOwnProperty('challenge')) {
@@ -88,6 +89,9 @@
                 return {
                     user: document.head.getAttribute('data-user-displayname')
                 };
+            },
+            getClasses() {
+                return '' + (this.hasPassword ? ' has-password':'') + (this.hasToken ? ' has-token':'');
             }
         },
 
@@ -111,7 +115,7 @@
             },
             loginError(e) {
                 this.password = '';
-                this.token = '';
+                if(this.provider.type !== 'user-token') this.token = '';
                 this.hasError = true;
 
                 if(e.status === 403) {
@@ -134,8 +138,7 @@
                 }, 250);
 
                 setTimeout(() => {
-                    document.body.classList.remove('pw-authorized');
-                    document.body.classList.remove('pw-authorisation');
+                    document.body.classList.remove('pw-authorize');
                 }, 500);
             },
             requestToken() {
@@ -165,7 +168,12 @@
 </script>
 
 <style lang="scss">
-    body#body-user.pw-authorisation {
+    body#body-user {
+        &:not(.pw-authorized) {
+            background-color : var(--color-primary);
+            background-image : var(--image-login-background);
+        }
+
         #header {
             background-color : rgba(0, 0, 0, 0);
         }
@@ -183,79 +191,44 @@
             z-index    : 1001;
             transition : transform ease-in-out 0.25s 0.25s;
         }
+    }
 
-        #app-content {
-            position         : fixed;
-            top              : 0;
-            left             : 0;
-            bottom           : 0;
-            right            : 0;
-            margin-left      : 0 !important;
-            display          : flex;
-            align-items      : center;
-            background-color : var(--color-primary);
-            background-image : var(--image-login-background);
-            opacity          : 1;
-            transition       : opacity ease-in-out 0.25s, margin-left ease-in-out 0.25s 0.25s;
+    #authorize-window {
+        margin     : 0 auto;
+        width      : 300px;
+        text-align : center;
+        position   : relative;
+
+        .fa-repeat {
+            position  : absolute;
+            font-size : 1.5rem;
+            color     : var(--color-primary-text);
+            padding   : 0.5rem;
+            cursor    : pointer;
+            right     : -2.5rem;
         }
 
-        #authorize-window {
-            margin     : 0 auto;
-            width      : 300px;
-            text-align : center;
-            position   : relative;
-
+        &.has-password {
             .fa-repeat {
-                position  : absolute;
-                font-size : 1.5rem;
-                color     : var(--color-primary-text);
-                padding   : 0.5rem;
-                cursor    : pointer;
-                right     : -2.5rem;
+                top : 3rem;
             }
+        }
 
-            input {
-                width     : 100%;
-                padding   : 0.75rem;
-                border    : none;
-                font-size : 1rem;
+        .login-container {
+            position : relative;
+        }
 
-                &[type=text],
-                &[type=password] {
-                    margin        : 0;
-                    box-shadow    : 0 1px 0 transparentize($color-black, 0.9) inset !important;
-                    border-radius : 0;
+        input {
+            width     : 100%;
+            padding   : 0.75rem;
+            border    : none;
+            font-size : 1rem;
 
-                    &:first-child {
-                        box-shadow              : none;
-                        border-top-left-radius  : 0.25rem;
-                        border-top-right-radius : 0.25rem;
-                    }
-
-                    &:last-child {
-                        border-bottom-left-radius  : 0.25rem;
-                        border-bottom-right-radius : 0.25rem;
-                    }
-                }
-
-                &[type=submit] {
-                    margin-top : 1rem;
-                    border     : 1px solid var(--color-primary-text);
-                    color      : var(--color-primary-text);
-                    background : var(--color-primary) url(/core/img/actions/confirm-white.svg?v=2) no-repeat right 16px center;
-                }
-            }
-
-            select {
-                width            : 100%;
-                border           : none;
-                font-size        : 1rem;
-                box-shadow       : 0 1px 0 transparentize($color-black, 0.9) inset !important;
-                border-radius    : 0;
-                background-color : var(--color-main-background);
-                padding          : 0.75rem 0.75rem 0.75rem 0.5rem;
-                margin           : 0;
-                height           : 2.75rem;
+            &[type=text],
+            &[type=password] {
+                margin        : 0;
+                box-shadow    : 0 1px 0 transparentize($color-black, 0.9) inset !important;
+                border-radius : 0;
 
                 &:first-child {
                     box-shadow              : none;
@@ -269,61 +242,114 @@
                 }
             }
 
-            .login-message {
-                padding          : 0.75rem 0.25rem 0.75rem 1.75rem;
-                margin-bottom    : 0.75rem;
-                background-color : var(--color-box-shadow);
-                color            : var(--color-primary-text);
-                font-weight      : bold;
-                font-size        : 1rem;
-                border-radius    : var(--border-radius);
-                position         : relative;
+            &[type=submit] {
+                margin-top : 1rem;
+                border     : 1px solid var(--color-primary-text);
+                color      : var(--color-primary-text);
+                background : var(--color-primary) url(/core/img/actions/confirm-white.svg?v=2) no-repeat right 16px center;
+            }
+        }
 
-                i {
-                    position    : absolute;
-                    left        : 0.5rem;
-                    top         : 0;
-                    bottom      : 0;
-                    display     : flex;
-                    align-items : center;
-                }
+        select {
+            width            : 100%;
+            border           : none;
+            font-size        : 1rem;
+            box-shadow       : 0 1px 0 transparentize($color-black, 0.9) inset !important;
+            border-radius    : 0;
+            background-color : var(--color-main-background);
+            padding          : 0.75rem 0.75rem 0.75rem 0.5rem;
+            margin           : 0;
+            height           : 2.75rem;
+
+            &:first-child {
+                box-shadow              : none;
+                border-top-left-radius  : 0.25rem;
+                border-top-right-radius : 0.25rem;
             }
 
-            .login-error {
-                color            : var(--color-error);
-                margin-top       : 1rem;
-                background-color : var(--color-box-shadow);
-                border-radius    : var(--border-radius);
-                padding          : 0.5rem;
-                border           : 1px solid var(--color-error);
-                position         : absolute;
-                width            : 300px;
-                animation        : shake 0.1s ease-in-out 0s 5;
+            &:last-child {
+                border-bottom-left-radius  : 0.25rem;
+                border-bottom-right-radius : 0.25rem;
+            }
+        }
 
-                @keyframes shake {
-                    0% {
-                        margin-left : -5px;
-                    }
-                    33% {
-                        margin-left : 0;
-                    }
-                    66% {
-                        margin-left : 5px;
-                    }
-                    99% {
-                        margin-left : 0;
-                    }
+        .login-message {
+            padding          : 0.75rem 0.25rem 0.75rem 1.75rem;
+            margin-bottom    : 0.75rem;
+            background-color : var(--color-box-shadow);
+            color            : var(--color-primary-text);
+            font-weight      : bold;
+            font-size        : 1rem;
+            border-radius    : var(--border-radius);
+            position         : relative;
+
+            i {
+                position    : absolute;
+                left        : 0.5rem;
+                top         : 0;
+                bottom      : 0;
+                display     : flex;
+                align-items : center;
+            }
+        }
+
+        .login-error {
+            color            : var(--color-error);
+            margin-top       : 1rem;
+            background-color : var(--color-box-shadow);
+            border-radius    : var(--border-radius);
+            padding          : 0.5rem;
+            border           : 1px solid var(--color-error);
+            position         : absolute;
+            width            : 300px;
+            animation        : shake 0.1s ease-in-out 0s 5;
+
+            @keyframes shake {
+                0% {
+                    margin-left : -5px;
+                }
+                33% {
+                    margin-left : 0;
+                }
+                66% {
+                    margin-left : 5px;
+                }
+                99% {
+                    margin-left : 0;
+                }
+            }
+        }
+    }
+
+
+    body#body-user {
+        &.pw-authorize {
+            #app-content {
+                position         : fixed;
+                top              : 0;
+                left             : 0;
+                bottom           : 0;
+                right            : 0;
+                margin-left      : 0 !important;
+                display          : flex;
+                align-items      : center;
+                background-color : var(--color-primary);
+                background-image : var(--image-login-background);
+                opacity          : 1;
+                transition       : opacity ease-in-out 0.25s, margin-left ease-in-out 0.25s 0.25s;
+            }
+            &.pw-authorized {
+                #app-content {
+                    margin-left : 0;
+                    opacity     : 0;
                 }
             }
         }
 
+
         &.pw-authorized {
             #app-navigation {
                 transform : translateX(0);
-            }
-            #app-content {
-                margin-left : 0;
-                opacity     : 0;
             }
             #header {
                 transition       : background-color ease-in-out 0.25s 0.25s;
