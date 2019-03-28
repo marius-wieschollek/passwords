a9ba2f0dbdbb858e5b57cf72448f18f9c12feb4b
Marius David Wieschollek <passwords.public@mdns.eu>
2019-03-28 12:09:26 +0100

    Load translations asynchronously

--- src/js/Classes/Localisation.js
+++ src/js/Classes/Localisation.js
@@ -7,8 +7,8 @@ export default class Localisation {
      * @returns {string}
      */
     static translate(text, variables = {}) {
-        if(text === undefined) return '';
-        if(OC !== undefined) return OC.L10N.translate('passwords', text, variables);
+        if (text === undefined) return '';
+        if (OC !== undefined) return OC.L10N.translate('passwords', text, variables);
 
         return '';
     }
@@ -33,5 +33,38 @@ export default class Localisation {
      */
     static formatDateTime(date) {
         return OC.Util.formatDate(date, 'LLL');
+    }
+
+    /**
+     *
+     * @param section
+     * @returns {Promise<any>}
+     */
+    static loadSection(section) {
+        let language = OC.getLanguage().replace('-', '_');
+        if (language === 'en') return;
+        let url = OC.filePath('passwords', 'l10n', `${section}/${language}.json`);
+
+        return new Promise((resolve, reject) => {
+            fetch(new Request(url))
+                .then((response) => {
+                    response.json()
+                        .then((d) => {
+                            if (response.ok) {
+                                if (d.translations) {
+                                    let translations = d.translations;
+                                    if(Array.isArray(translations)) {
+                                        translations = Object.assign.apply(this, translations);
+                                    }
+
+                                    OC.L10N.register('passwords', translations, d.pluralForm);
+                                }
+                                resolve();
+                            } else {
+                                reject(d);
+                            }
+                        });
+                });
+        });
     }
 }
--- src/js/Manager/SetupManager.js
+++ src/js/Manager/SetupManager.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import SettingsManager from '@js/Manager/SettingsManager';
+import Localisation from "@/js/Classes/Localisation";
 
 class SetupManager {
 
     async run() {
         if(SettingsManager.get('client.setup.initialized', false)) return;
+        await Localisation.loadSection('tutorial');
 
         let SetupDialog = await import(/* webpackChunkName: "SetupWizard" */ '@vue/Dialog/SetupDialog.vue'),
             SetupWizard = Vue.extend(SetupDialog.default);
