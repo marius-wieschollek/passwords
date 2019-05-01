424c8247835bcc1179e09d408a123c0142f0f102
Marius David Wieschollek <passwords.public@mdns.eu>
2019-05-01 22:17:53 +0200

    Fix error in changing of editable status of shared passwords

--- src/lib/Cron/SynchronizeShares.php
+++ src/lib/Cron/SynchronizeShares.php
@@ -343,7 +343,7 @@ class SynchronizeShares extends AbstractCronJob {
                     break;
                 }
 
-                if(!$share->isShareable() && !$share->isEditable()) {
+                if(!$share->isEditable()) {
                     $subShares = $this->shareService->findBySourcePassword($password->getUuid());
                     foreach($subShares as $subShare) {
                         if($subShare->isEditable()) {
@@ -352,7 +352,6 @@ class SynchronizeShares extends AbstractCronJob {
                             $this->shareService->save($subShare);
                         }
                     }
-                    break;
                 }
             }
         } while($count !== 0);
