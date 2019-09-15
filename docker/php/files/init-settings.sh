faf57c26d438d122a631856ae55cf57e58bd1d90
Marius David Wieschollek <passwords.public@mdns.eu>
2019-09-15 10:32:06 +0200

    Added local domain
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- docker/php/files/init-settings.sh
+++ docker/php/files/init-settings.sh
@@ -9,3 +9,6 @@
 # System Settings
 /var/www/html/occ config:system:set loglevel --value=0 --type=int;
 /var/www/html/occ config:system:set defaultapp --value=passwords --type=string;
+/var/www/html/occ config:system:set trusted_domains 0 --value=localhost --type=string;
+/var/www/html/occ config:system:set trusted_domains 1 --value=passwords.local --type=string;
+/var/www/html/occ config:system:set overwrite.cli.url --value=https://localhost --type=string;
