0872a5681a50d99912e4e20fa67ec0d6edcd2f7b
Marius David Wieschollek <passwords.public@mdns.eu>
2020-05-17 22:22:40 +0200

    Improve QR code compatibility
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/vue/Details/Password/QrCode.vue
+++ src/vue/Details/Password/QrCode.vue
@@ -6,7 +6,7 @@
             <translate tag="option" value="url" v-if="password.url" say="Website"/>
             <translate tag="option" value="hash" say="SHA-1"/>
         </select>
-        <qr-code :text="text" :color="color" :size="256" bgColor="#fff0" errorLevel="H"/>
+        <qr-code :text="text" :color="color" :size="256" bgColor="#fff0" errorLevel="Q"/>
     </div>
 </template>
 
