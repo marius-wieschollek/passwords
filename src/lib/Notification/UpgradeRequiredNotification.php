af0eb9e3d6a5afcfeaadb6b302a1599182e38925
Marius David Wieschollek <passwords.public@mdns.eu>
2020-11-14 00:33:07 +0100

    Update eol notification text
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/lib/Notification/UpgradeRequiredNotification.php
+++ src/lib/Notification/UpgradeRequiredNotification.php
@@ -8,6 +8,7 @@
 namespace OCA\Passwords\Notification;
 
 use OCP\IL10N;
+use OCP\Notification\IAction;
 use OCP\Notification\INotification;
 
 /**
@@ -19,7 +20,7 @@ class UpgradeRequiredNotification extends AbstractNotification {
 
     const NAME                           = 'upgrade_required';
     const TYPE                           = 'admin';
-    const MANUAL_URL_SYSTEM_REQUIREMENTS = 'https://git.mdns.eu/nextcloud/passwords/wikis/Administrators/System-Requirements';
+    const MANUAL_URL_SYSTEM_REQUIREMENTS = 'https://git.mdns.eu/nextcloud/passwords/wikis/Administrators/Notifications/Platform-Support-Notification';
 
     /**
      * Send the notification
@@ -32,6 +33,12 @@ class UpgradeRequiredNotification extends AbstractNotification {
             = $this->createNotification($userId)
                    ->setSubject(self::NAME, $parameters)
                    ->setObject('app', 'requirements');
+
+        $linkAction = $notification->createAction();
+        $linkAction->setLabel('information')
+                   ->setLink(self::MANUAL_URL_SYSTEM_REQUIREMENTS, IAction::TYPE_WEB)
+                   ->setPrimary(true);
+        $notification->addAction($linkAction);
 
         $this->notificationManager->notify($notification);
     }
@@ -48,6 +55,11 @@ class UpgradeRequiredNotification extends AbstractNotification {
         $title   = $this->getTitle($localisation);
         $message = $this->getMessage($localisation, $notification->getSubjectParameters());
 
+        foreach($notification->getActions() as $action) {
+            $action->setLink(self::MANUAL_URL_SYSTEM_REQUIREMENTS, IAction::TYPE_WEB)->setParsedLabel($localisation->t('More information'));
+            $notification->addParsedAction($action);
+        }
+
         return $notification
             ->setParsedSubject($title)
             ->setParsedMessage($message)
@@ -60,7 +72,7 @@ class UpgradeRequiredNotification extends AbstractNotification {
      * @return string
      */
     protected function getTitle(IL10N $localisation): string {
-        return $localisation->t('The passwords app will end support for your platform');
+        return $localisation->t('The passwords app will discontinue updates for your platform');
     }
 
     /**
@@ -70,8 +82,10 @@ class UpgradeRequiredNotification extends AbstractNotification {
      * @return string
      */
     protected function getMessage(IL10N $localisation, array $params): string {
-        return $localisation->t('The upcoming version %s of Passwords will require at least Nextcloud %s and PHP %s.', $params).
+        return $localisation->t('Passwords %s will raise the minimum system requirements to Nextcloud %s and PHP %s.', $params).
                ' '.
-               $localisation->t('To keep receiving security and feature updates, please update your server.');
+               $localisation->t('We recommend that you update your server so you don\'t miss out on future updates.').
+               ' '.
+               $localisation->t('Your installed version will continue to work regardless and you can still use it as it is.');
     }
 }
