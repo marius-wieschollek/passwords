c8d95a468ce5896379fddbe0142727b4ec6c2f3b
Marius David Wieschollek <passwords.public@mdns.eu>
2021-06-04 09:19:33 +0200

    [#387] Remove function call
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/lib/Migration/CheckAppRequirements.php
+++ src/lib/Migration/CheckAppRequirements.php
@@ -60,7 +60,6 @@ class CheckAppRequirements implements IRepairStep {
      */
     public function run(IOutput $output) {
         $this->canInstallRelease();
-        $this->canUpgradeFromPreviousVersion();
     }
 
     /**
