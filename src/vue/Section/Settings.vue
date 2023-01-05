467eaeed3e5d079e50b356c8df077c881e72e865
Marius David Wieschollek <passwords.public@mdns.eu>
2023-01-05 15:04:35 +0100

    Update copyright
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/vue/Section/Settings.vue
+++ src/vue/Section/Settings.vue
@@ -1,3 +1,13 @@
+<!--
+  - @copyright 2023 Passwords App
+  -
+  - @author Marius David Wieschollek
+  - @license AGPL-3.0
+  -
+  - This file is part of the Passwords App
+  - created by Marius David Wieschollek.
+  -->
+
 <template>
     <div id="app-content">
         <div class="app-content-left settings">
