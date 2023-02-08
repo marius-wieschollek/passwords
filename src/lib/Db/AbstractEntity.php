775841618983f0106c08fcf06bc50404d02de155
Marius David Wieschollek <passwords.public@mdns.eu>
2023-02-08 09:27:43 +0100

    Fix issues with trait
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/lib/Db/AbstractEntity.php
+++ src/lib/Db/AbstractEntity.php
@@ -1,8 +1,12 @@
 <?php
-/**
+/*
+ * @copyright 2023 Passwords App
+ *
+ * @author Marius David Wieschollek
+ * @license AGPL-3.0
+ *
  * This file is part of the Passwords App
- * created by Marius David Wieschollek
- * and licensed under the AGPL.
+ * created by Marius David Wieschollek.
  */
 
 namespace OCA\Passwords\Db;
--- src/lib/Db/Traits/GetterSetterTrait.php
+++ src/lib/Db/Traits/GetterSetterTrait.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * @copyright 2023 Passwords App
+ *
+ * @author Marius David Wieschollek
+ * @license AGPL-3.0
+ *
+ * This file is part of the Passwords App
+ * created by Marius David Wieschollek.
+ */
 
 namespace OCA\Passwords\Db\Traits;
 
