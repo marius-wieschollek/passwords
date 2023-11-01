cc3a44149097c87e681ff85bc22da44e1661ce5d
Marius David Wieschollek <passwords.public@mdns.eu>
2023-11-01 16:19:19 +0100

    Add title for the webauthn dialog
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/js/Actions/WebAuthn/InitializeWebAuthnAction.js
+++ src/js/Actions/WebAuthn/InitializeWebAuthnAction.js
@@ -72,6 +72,7 @@ export default class InitializeWebAuthnAction {
                     }
                 }
             },
+            'WebauthnEncryptionSetupTitle',
             'WebauthnEncryptionPassphraseText'
         );
     }
