d2139bdb77c36b5f6a18e5f8035e3b61cd933683
Marius David Wieschollek <passwords.public@mdns.eu>
2024-11-20 13:28:17 +0100

    Throttle favicon requests
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/js/Services/FaviconService.js
+++ src/js/Services/FaviconService.js
@@ -1,5 +1,5 @@
-import API from '@js/Helper/api';
 import SettingsService from "@js/Services/SettingsService";
+import ClientService from "@js/Services/ClientService";
 
 export default new class FaviconService {
 
@@ -7,8 +7,7 @@ export default new class FaviconService {
      *
      */
     constructor() {
-        this._favicons = {};
-        this._requests = {};
+        this._service = null;
     }
 
     /**
@@ -16,14 +15,10 @@ export default new class FaviconService {
      *
      * @param {String} domain
      * @param {Number} size
-     * @return {Promise<String>}
+     * @return {String}
      */
     get(domain, size = 32) {
-        if(this._favicons.hasOwnProperty(`${domain}_${size}`)) {
-            return this._favicons[`${domain}_${size}`];
-        }
-
-        return SettingsService.get('server.theme.app.icon');
+        return this._getService().get(domain, size);
     }
 
     /**
@@ -32,70 +27,23 @@ export default new class FaviconService {
      * @param {Number} size
      * @return {Promise<String>}
      */
-    async fetch(domain, size = 32) {
-        if(this._favicons.hasOwnProperty(`${domain}_${size}`)) {
-            return this._favicons[`${domain}_${size}`];
-        }
-
-        if(this._requests.hasOwnProperty(`${domain}_${size}`)) {
-            await this._requests[`${domain}_${size}`];
-            return this._favicons[`${domain}_${size}`];
-        }
-
-        let request = this._queueApiRequest(domain, size);
-
-        return await request;
+    fetch(domain, size = 32) {
+        return this._getService().fetch(domain, size);
     }
 
     /**
      *
-     * @param {String} domain
-     * @param {Number} size
-     * @return {Promise<*>}
+     * @return {FaviconService}
      * @private
      */
-    async _queueApiRequest(domain, size) {
-        let promise = this._fetchFromApi(domain, size);
+    _getService() {
+        if(this._service === null) {
+            let fallbackIcon = SettingsService.get('server.theme.app.icon');
 
-        this._requests[`${domain}_${size}`] = promise;
-
-        return promise;
-    }
-
-    /**
-     *
-     * @param {String} domain
-     * @param {Number} size
-     * @return {Promise<*>}
-     * @private
-     */
-    async _fetchFromApi(domain, size) {
-        let fallbackIcon = SettingsService.get('server.theme.app.icon');
-        try {
-            /** @type {Blob} favicon **/
-            let favicon = await API.getFavicon(domain, size);
-
-            if(favicon.type.substr(0, 6) !== 'image/' || favicon.size < 1) {
-                delete this._requests[`${domain}_${size}`];
-
-                if(!this._favicons.hasOwnProperty(`${domain}_${size}`)) {
-                    this._favicons[`${domain}_${size}`] = fallbackIcon;
-                }
-                return fallbackIcon;
-            }
-
-            this._favicons[`${domain}_${size}`] = window.URL.createObjectURL(favicon);
-            delete this._requests[`${domain}_${size}`];
-
-            return this._favicons[`${domain}_${size}`];
-        } catch(e) {
-            if(this._requests.hasOwnProperty(`${domain}_${size}`)) {
-                delete this._requests[`${domain}_${size}`];
-            }
-            if(!this._favicons.hasOwnProperty(`${domain}_${size}`)) {
-                this._favicons[`${domain}_${size}`] = fallbackIcon;
-            }
-            return fallbackIcon;
+            /** @type FaviconService **/
+            this._service = ClientService.getClient().getInstance('service.favicon', fallbackIcon);
         }
+
+        return this._service;
     }
 };
