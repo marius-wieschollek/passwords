fa0aa98bead7ba980ed1eebf3432bf7d0de95fd5
Marius David Wieschollek <passwords.public@mdns.eu>
2024-11-24 00:22:11 +0100

    Fix NC 30 ui style
    Signed-off-by: Marius David Wieschollek <passwords.public@mdns.eu>

--- src/vue/Components/Navigation/AppNavigation.vue
+++ src/vue/Components/Navigation/AppNavigation.vue
@@ -10,50 +10,50 @@
 
 <template>
     <nc-app-navigation>
-        <app-navigation-search />
+        <app-navigation-search/>
         <template id="app-passwords-navigation" #list>
             <app-navigation-item :name="t('All')" :to="{ name: 'All'}" :exact="true">
                 <template #icon>
-                    <earth-icon :size="20" />
+                    <earth-icon :size="20"/>
                 </template>
             </app-navigation-item>
-            <app-navigation-item-folders />
+            <app-navigation-item-folders/>
             <app-navigation-item :name="t('Recent')" :to="{ name: 'Recent'}">
                 <template #icon>
-                  <clock-icon :size="20" />
+                    <clock-icon :size="20"/>
                 </template>
             </app-navigation-item>
-            <app-navigation-item-favorites />
-            <app-navigation-item-shared />
-            <app-navigation-item-tags />
-            <app-navigation-item-security />
+            <app-navigation-item-favorites/>
+            <app-navigation-item-shared/>
+            <app-navigation-item-tags/>
+            <app-navigation-item-security/>
             <app-navigation-item :name="t('Search')" :to="{ name: 'Search'}" v-if="isSearchVisible">
                 <template #icon>
-                    <magnify-icon :size="20" />
+                    <magnify-icon :size="20"/>
                 </template>
             </app-navigation-item>
 
-            <session-timeout v-if="!isMobile && hasTimeout" />
+            <session-timeout v-if="!isMobile && hasTimeout"/>
         </template>
         <template #footer>
-            <app-navigation-more />
+            <app-navigation-more/>
         </template>
     </nc-app-navigation>
 </template>
 <script>
-    import NcAppNavigation            from '@nc/NcAppNavigation.js';
-    import AppNavigationItem          from '@vc/Navigation/AppNavigationItem';
+    import NcAppNavigation from '@nc/NcAppNavigation.js';
+    import AppNavigationItem from '@vc/Navigation/AppNavigationItem';
     import AppNavigationItemFavorites from '@vc/Navigation/AppNavigationItemFavorites';
-    import AppNavigationItemFolders   from '@vc/Navigation/AppNavigationItemFolders';
-    import AppNavigationItemSecurity  from '@vc/Navigation/AppNavigationItemSecurity';
-    import AppNavigationItemShared    from '@vc/Navigation/AppNavigationItemShared';
-    import AppNavigationItemTags      from '@vc/Navigation/AppNavigationItemTags';
-    import AppNavigationMore          from '@vc/Navigation/AppNavigationMore';
-    import AppNavigationSearch        from '@vc/Navigation/AppNavigationSearch.vue';
-    import SessionTimeout             from '@vc/SessionTimeout';
-    import ClockIcon                  from '@icon/Clock';
-    import EarthIcon                  from '@icon/Earth';
-    import MagnifyIcon                from '@icon/Magnify';
+    import AppNavigationItemFolders from '@vc/Navigation/AppNavigationItemFolders';
+    import AppNavigationItemSecurity from '@vc/Navigation/AppNavigationItemSecurity';
+    import AppNavigationItemShared from '@vc/Navigation/AppNavigationItemShared';
+    import AppNavigationItemTags from '@vc/Navigation/AppNavigationItemTags';
+    import AppNavigationMore from '@vc/Navigation/AppNavigationMore';
+    import AppNavigationSearch from '@vc/Navigation/AppNavigationSearch.vue';
+    import SessionTimeout from '@vc/SessionTimeout';
+    import ClockIcon from '@icon/Clock';
+    import EarthIcon from '@icon/Earth';
+    import MagnifyIcon from '@icon/Magnify';
 
     export default {
         components: {
@@ -78,3 +78,10 @@
         }
     };
 </script>
+
+<style lang="scss">
+.app-navigation-list {
+    flex-grow : 1;
+    height    : auto !important;
+}
+</style>
